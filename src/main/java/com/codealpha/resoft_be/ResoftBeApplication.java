887fc0f89dfdd085e9af31494614b3c369d8edb4
KNU-K <knukang334@gmail.com>
2024-09-10 23:57:52 +0900

    init : initialize spring with java

---
+++ src/main/java/com/codealpha/resoft_be/ResoftBeApplication.java
@@ -0,0 +1,13 @@
+package com.codealpha.resoft_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResoftBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResoftBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codealpha/resoft_be/ResoftBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codealpha.resoft_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResoftBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

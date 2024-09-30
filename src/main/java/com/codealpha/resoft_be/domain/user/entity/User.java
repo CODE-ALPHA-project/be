9ab08d717757b47b5146e61970f25577ee9d2909
KNU-K <knukang334@gmail.com>
2024-09-30 14:12:27 +0900

    feat : add feature about user domain simply.

---
+++ src/main/java/com/codealpha/resoft_be/domain/user/entity/User.java
@@ -0,0 +1,33 @@
+package com.codealpha.resoft_be.domain.user.entity;
+
+import com.codealpha.resoft_be.domain.userchatroom.entity.UserChatroom;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "users")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column()
+    String name;
+
+    @Column()
+    String email;
+
+    @Column()
+    String password;
+
+    @OneToMany
+    @Builder.Default
+    List<UserChatroom> userChatroomList = new ArrayList<>();
+}

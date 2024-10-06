0c0d1c70af06f15ec8652e589c18f6d45f923302
KNU-K <knukang334@gmail.com>
2024-10-06 15:37:09 +0900

    refactor : change sync to async. - MongoRepository -> ReactiveMongoRepository
    * This solution is faster than async method.

--- src/main/java/com/codealpha/resoft_be/domain/message/repository/MessageRepository.java
+++ src/main/java/com/codealpha/resoft_be/domain/message/repository/MessageRepository.java
@@ -1,10 +1,10 @@
 package com.codealpha.resoft_be.domain.message.repository;
 
 import com.codealpha.resoft_be.domain.message.entity.Message;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository extends MongoRepository<Message, String> {
+public interface MessageRepository extends ReactiveMongoRepository<Message, String> {
     // 추가적인 쿼리 메서드를 정의할 수 있습니다.
 }

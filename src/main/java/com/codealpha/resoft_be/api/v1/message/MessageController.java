0fdd5fedc59726a4b6096861d4d1050e9054b968
KNU-K <knukang334@gmail.com>
2024-10-07 14:44:39 +0900

    feat : add MessageController v1 it is just simply feature that is endpoint for receiving all the chat history.

--- src/main/java/com/codealpha/resoft_be/api/v1/message/MessageController.java
+++ src/main/java/com/codealpha/resoft_be/api/v1/message/MessageController.java
@@ -1,26 +1,31 @@
-package com.codealpha.resoft_be.api.v1.message;
+    package com.codealpha.resoft_be.api.v1.message;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-import reactor.core.publisher.Mono;
+    import com.codealpha.resoft_be.domain.message.dto.Request;
+    import com.codealpha.resoft_be.domain.message.service.MessageService;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.messaging.handler.annotation.MessageMapping;
+    import org.springframework.messaging.handler.annotation.SendTo;
+    import org.springframework.messaging.simp.SimpMessagingTemplate;
+    import org.springframework.stereotype.Controller;
+    import reactor.core.publisher.Mono;
 
-/**
- * @author knu-k
- * this Controller is used for reactive messaging
- */
-@Controller
-@RequiredArgsConstructor
-public class MessageController {
-    private final SimpMessagingTemplate messagingTemplate;
+    /**
+     * @author knu-k
+     * This Controller is used for reactive messaging
+     */
+    @Controller
+    @RequiredArgsConstructor
+    public class MessageController {
+        private final SimpMessagingTemplate messagingTemplate;
+        private final MessageService messageService;
 
-    @MessageMapping("/send")
-    @SendTo("/topic/messages")
-    public Mono<String> a(String message) {
-        String responseMessage = "Echo: " + message;
-        messagingTemplate.convertAndSend("/topic/messages", responseMessage);
-        return Mono.just("하이");
+        @MessageMapping("/send")
+//        @SendTo("/topic/messages")
+        public void send(Request.Send sendMessage) { // Change parameter type to DTO
+
+            messageService.saveMessage(sendMessage);
+
+            String responseMessage = "Echo: " + sendMessage.getMessage();
+            messagingTemplate.convertAndSend("/topic/messages", responseMessage);
+        }
     }
-}

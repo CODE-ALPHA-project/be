1d46d2e3def3e1c73c081cae5f24833fec84cf7a
KNU-K <knukang334@gmail.com>
2024-10-15 05:23:09 +0900

    feat : add custom global exception handler by using ControllerAdvice

---
+++ src/main/java/com/codealpha/resoft_be/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,6 @@
+package com.codealpha.resoft_be.common.handler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}

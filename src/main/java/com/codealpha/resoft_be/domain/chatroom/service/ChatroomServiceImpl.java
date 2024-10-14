37c437198d50cac9eadf2e10cb6eb16a3cc5de01
KNU-K <knukang334@gmail.com>
2024-10-15 05:21:55 +0900

    refactor : change RuntimeException to UserNotFoundException.java

--- src/main/java/com/codealpha/resoft_be/domain/chatroom/service/ChatroomServiceImpl.java
+++ src/main/java/com/codealpha/resoft_be/domain/chatroom/service/ChatroomServiceImpl.java
@@ -4,6 +4,7 @@ import com.codealpha.resoft_be.domain.chatroom.dto.Request;
 import com.codealpha.resoft_be.domain.chatroom.entity.Chatroom;
 import com.codealpha.resoft_be.domain.chatroom.repository.ChatroomRepository;
 import com.codealpha.resoft_be.domain.user.entity.User;
+import com.codealpha.resoft_be.domain.user.exception.UserNotFoundException;
 import com.codealpha.resoft_be.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,7 +24,7 @@ public class ChatroomServiceImpl implements ChatroomService{
     @Override
     public Chatroom createChatroom(Request.Create createCmd) {
         User foundUser = userRepository.findById(createCmd.getUserId())
-                .orElseThrow(()-> new RuntimeException("err "));
+                .orElseThrow(()-> new UserNotFoundException("User not found"));
 
         Chatroom chatroom = Chatroom.create(createCmd.getName(), createCmd.getType(), createCmd.getStatus());
         chatroom.addParticipant(foundUser);
